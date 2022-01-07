9bacdd8b564ac498908327481a55117a6d67d8de
itsusinn <itsusinn@foxmail.com>
2022-01-07 23:09:18 +0800

    ci: auto release

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
   id("org.jetbrains.kotlin.jvm") version ("1.6.0")
   id("org.jetbrains.kotlin.plugin.serialization")version ("1.6.0")
   id("com.github.johnrengelman.shadow")version ("7.1.0")
-  // id("org.meowcat.kato") version "0.1.0-dev24"
+  id("org.meowcat.kato") version "0.1.0-dev31"
 }
 
 group = property("maven_group")!!
@@ -12,20 +12,68 @@ version = property("mod_version")!!
 repositories {
   mavenCentral()
   maven("https://jitpack.io")
+  mavenLocal()
 }
-
+kato {
+  excludePath("kotlin/*")
+  excludePath("kotlinx/coroutines/*")
+  excludePath("kotlinx/serialization/*")
+  excludePath("mappings/*")
+  excludePath("META-INF/*.kotlin_module")
+  excludePath("*.md")
+  excludePath("DebugProbesKt.bin")
+  excludeGroups(
+    "org.lwjgl",
+    "net.fabricmc",
+    "org.ow2.asm",
+    "net.minecraft",
+    "com.mojang",
+    "net.java.dev.jna",
+    "com.google.jimfs",
+    "org.jetbrains.kotlinx",
+    "org.jetbrains.kotlin",
+    "org.apache.logging.log4j",
+    "net.java.jinput",
+    "net.java.jutils",
+    "org.apache.commons",
+    "org.apache.httpcomponents",
+    "commons-logging",
+    "com.ibm.icu",
+    "it.unimi.dsi",
+    "ca.weblite",
+    "net.minecrell",
+    "com.google.guava",
+    "commons-io",
+    "commons-codec",
+    "com.google.code.gson",
+    "oshi-project",
+    "io.netty",
+    "net.sf.jopt-simple",
+    "net_fabricmc_yarn_1_16_5_1_16_5_build_1_v2.net.fabricmc.fabric-api",
+    "net_fabricmc_yarn_1_16_5_1_16_5_build_1_v2.net.fabricmc",
+  )
+  shadowJar {
+    minimize()
+  }
+}
+java {
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(16))
+  }
+}
 dependencies {
   minecraft("com.mojang:minecraft:${property("minecraft_version")}")
   mappings("net.fabricmc:yarn:${property("yarn_mappings")}:v2")
 
-  modImplementation("net.fabricmc:fabric-loader:${property("loader_version")}")
+  modCompileClasspath("net.fabricmc:fabric-loader:${property("loader_version")}")
 
   modImplementation("net.fabricmc:fabric-language-kotlin:${property("fabric_kotlin_version")}")
   modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_api_version")}")
 
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.0")
+  compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.0")
   implementation("io.nats:jnats:2.13.1")
-  implementation("org.meowcat:mesagisto-client-jvm:1.0.12")
+  implementation("org.meowcat:mesagisto-client-jvm:1.1.0")
+  // implementation("org.meowcat:mesagisto-client:1.0.14-build")
   implementation("io.arrow-kt:arrow-core:1.0.0")
   implementation("com.charleskorn.kaml:kaml:0.37.0")
 }
