5a54e54b63c689582f719e21f157674022f9eaea
itsusinn <itsusinn@foxmail.com>
2022-04-30 14:24:25 +0800

    feat: support 1.18

--- build.sh
+++ build.sh
@@ -1,11 +1,28 @@
-build-1_16_5(){
-	cd 1_16_5
+build-1_16(){
+	cd 1_16
 	bash build.sh
 	cd ..
-	mv 1_16_5/packages/* packages/
+	mv 1_16/packages/* packages/
+}
+build-1_17(){
+	cd 1_17
+	bash build.sh
+	cd ..
+	mv 1_17/packages/* packages/
+}
+build-1_18(){
+	cd 1_18
+	bash build.sh
+	cd ..
+	mv 1_18/packages/* packages/
 }
 build(){
 	mkdir packages
-	build-1_16_5
+	./gradlew clean :1_16:remapJar
+	build-1_16
+	./gradlew clean :1_17:remapJar
+	build-1_17
+	./gradlew clean :1_18:remapJar
+	build-1_18
 }
 build
