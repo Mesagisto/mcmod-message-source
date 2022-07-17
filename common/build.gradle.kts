bd2d5da9b04ca4541d0ed2fc6b58d93e939a6960
Itsusinn <itsusinn@meowcat.org>
2022-07-17 21:17:34 +0800

    fix: missing shade deps

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -18,7 +18,15 @@ dependencies {
   modCompileOnly("net.fabricmc:fabric-loader:0.11.3")
   modCompileOnly("net.fabricmc.fabric-api:fabric-api:${property("fabric_api_version")}")
 
+  implementation("org.fusesource.leveldbjni:leveldbjni-all:1.8")
+
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.2")
+
+  implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.3")
+  implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-cbor:2.13.3")
+
   implementation("io.nats:jnats:2.15.3")
+  implementation("org.bouncycastle:bcprov-jdk15on:1.70")
   implementation("org.mesagisto:mesagisto-client:1.5.1")
 }
