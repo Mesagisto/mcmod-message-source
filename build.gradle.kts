c3981993008b2d48be1ef97ab78803cf46360da1
Itsusinn <itsusinn@meowcat.org>
2023-01-15 13:42:34 +0800

    chore: release 1.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,13 @@ plugins {
   id("dev.architectury.loom") version "1.0-SNAPSHOT" apply false
   id("architectury-plugin") version "3.4-SNAPSHOT"
 
-  id("org.jetbrains.kotlin.jvm") version ("1.7.0")
+  id("org.jetbrains.kotlin.jvm") version ("1.6.0")
   id("com.github.johnrengelman.shadow")version ("7.1.0")
   id("io.itsusinn.pkg") version "1.2.2"
 }
 allprojects {
   group = "org.mesagisto"
-  version = "1.2.2"
+  version = "1.3.0"
   repositories {
     mavenLocal()
     mavenCentral()
